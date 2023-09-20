569b7d5c0ec5663be96da6e048e3ea4d35b57718
JsFrancisc0 <j.saldias03@ufromail.cl>
2023-09-20 16:02:07 -0300

    uwu

---
+++ src/main/java/Television.java
@@ -0,0 +1,31 @@
+public class Television {
+
+    String marca, color, modelo;
+    int resolucion;
+    boolean smarttv;
+
+
+    public void encendr(){
+    //beeep
+    }
+
+    public void cambiarCanal(){
+    //click click
+    }
+
+    public void subirVolumen(){
+    //AAAAAAAAAAAHHHHHHHHHHHH
+    }
+
+    public void bajarVolumen(){
+    //aaaaaaaaaaaahhhhhhhhhhh
+    }
+
+    public void menuApps(){
+    //TU-DUM! *ese sonido que hace Netflix*
+    }
+
+    public void apagar(){
+    //adios
+    }
+}
